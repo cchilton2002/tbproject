e8d294f20bcff9a8e660ce0b31c06e7280ca2178
cchilton2002 <christoph0295@gmail.com>
2024-11-18 19:24:24 +0000

    Website route amendment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './components/Login';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Register from './components/Register';
 import Dashboard from './components/Dashboard/Dashboard';
 import Tasks from './components/Dashboard/Tasks';
