d1ee3cf2229aa809a1029227a53dbf97722a2c21
cchilton2002 <christoph0295@gmail.com>
2024-11-18 20:22:18 +0000

    Changes to the football component

--- src/components/Sports.jsx
+++ src/components/Sports.jsx
@@ -11,7 +11,7 @@ const Sports = () => {
   useEffect(() => {
     const fetchTeamWins = async () => {
       try {
-        const response = await fetch("/data/team_wins.json");  
+        const response = await fetch("../assets/team_wins.json");  
         if (!response.ok) {
           throw new Error(`Failed to fetch: ${response.statusText}`);
         }
